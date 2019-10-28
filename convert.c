38126cbc52467cc871e5a260027c7ead796ed2d9
danny099 <dalmariv01@gmail.com>
2019-10-28 14:38:37 +0000

    change the name

---
+++ convert.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * convert_c - function
+ *
+ *@symbol: is a pointer
+ *@list: is a va_list
+ * Return: On succes .
+ * On error.
+ */
+void (*convert_c(const char *symbol))(va_list list)
+{
+	int i;
+
+	op_t ops[] = {
+		{"c", print_c},
+		{"d", print_d},
+		{"s", print_s},
+		{"i", print_i},
+	};
+
+	for (i = 0; i < 4; i++)
+	{
+		if (*symbol == *(ops[i].op))
+			return ops[i].f;
+	}
+	write(2, "Not found", 9);
+	exit(1);
+}
