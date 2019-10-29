81312f71242322cdfe43cc1d5735a8261bab278c
FeliPrado31 <srgatosicool@gmail.com>
2019-10-29 15:17:56 +0000

    impement buffesito to store overall of the strings

--- _printf.c
+++ _printf.c
@@ -2,23 +2,38 @@
 
 int _printf(const char *format, ...)
 {
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return(-1);
-
-	int i;
-
-
 	va_list args;
+	char *buffersito, *buffer_extra;
+	int i, count = 0;
+	int add = 0;
+	buffersito = malloc(sizeof(char) * 1024);
+	if (buffersito == NULL || format == NULL)
+		return (-1);
+	buffer_extra = buffersito;
 
 	va_start(args, format);
-
-	for (i = 0; *(format + i) != '\0'; i++)
+	for (i = 0; *(format + i); i++)
 	{
 		if (*(format + i) == '%')
-			convert_c(format + ++i)(args);
-		else
-			write(1, (format + i), 1);
+		{
+			i++;
+				if (*(format + i) != '%')
+				{
+					add = type(format + i)(args, buffersito);
+					count += add;
+					buffersito += add;
+					i++;
+				}
+		}
+		*buffersito = *(format + i);
+		count++;
+		buffersito++;
 	}
+	*buffersito = '\0';
+
+	write(1, buffer_extra, count);
 	va_end(args);
-	return (i);
+	free(buffer_extra);
+
+	return (count);
 }
