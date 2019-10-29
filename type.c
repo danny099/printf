0b67d93989556558f484985972621b293315cd1f
FeliPrado31 <srgatosicool@gmail.com>
2019-10-29 18:53:59 +0000

    change the prototype

--- type.c
+++ type.c
@@ -1,17 +1,15 @@
 #include "holberton.h"
 
-int (*type(const char *simbolo))(va_list va, char *buffersito)
+int (*type(const char *simbolo))(va_list va, char *buffer)
 {
 	op_t func[] = {
 		{"c", print_c},
-		{"s", print_s},/*
-		{"d", print_d_i},
-		{"i", print_d_i},*/
-		{"%", print_por}
+		{"s", print_s},
+		{"%", print_percent}
 	};
 	int i;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 3; i++)
 	{
 		if (*simbolo == *(func[i].string))
 			return (func[i].f);
