494feaae12818859fc4501d32fe576bfdb1f76f2
FeliPrado31 <srgatosicool@gmail.com>
2019-10-29 18:42:56 +0000

    added new var for lenght and size

--- _printf.c
+++ _printf.c
@@ -4,8 +4,8 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	char *buffersito, *buffer_extra;
-	int i, count = 0;
-	int add = 0;
+	int i, count = 0, lenght = 0, size = 0;
+
 	buffersito = malloc(sizeof(char) * 1024);
 	if (buffersito == NULL || format == NULL)
 		return (-1);
@@ -24,17 +24,19 @@ int _printf(const char *format, ...)
 			if (type(format + i + 1))
 			{
 				i++;
-				add = type(format + i)(args, buffersito);
-				count += add;
-				buffersito += add;
+				size = type(format + i)(args, buffersito);
+				buffersito += size;
+				lenght += size;
+				count = lenght;
 				continue;
 			}
 		}
 		*buffersito = *(format + i);
-		count++;
+		lenght++;
 		buffersito++;
+		count = lenght;
 	}
-	write(1, buffer_extra, count);
+	write(1, buffer_extra, lenght);
 	va_end(args);
 	free(buffer_extra);
 
