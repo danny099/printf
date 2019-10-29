654af44e2d31e44fb6211f779068940e71053739
FeliPrado31 <srgatosicool@gmail.com>
2019-10-29 19:47:11 +0000

    change all

--- holberton.h
+++ holberton.h
@@ -8,15 +8,12 @@
 typedef struct formatter
 {
 	char *string;
-	int (*f)(va_list va, char *buffersito);
+	int (*f)(va_list va);
 } op_t;
 
 int _printf(const char *format, ...);
-int (*type(const char *symbol))(va_list va, char *buffersito);
-void returnNumber(int n, char *buffersito);
-int print_c(va_list va, char *buffersito);
-int print_s(va_list va, char *buffersito);
-int print_d_i(va_list va, char *buffersito);
-int print_por(va_list va, char *buffersito);
+int (*type(const char *symbol))(va_list va);
+int print_c(va_list va);
+int print_s(va_list va);
 
 #endif
