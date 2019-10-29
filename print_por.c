e6ba9a585d8e4c2a8ebeabd7fc0f0a35bbce9ff0
danny099 <dalmariv01@gmail.com>
2019-10-29 15:50:26 +0000

    chage the name fuction print_por

--- print_por.c
+++ print_por.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-int print_percent(va_list __attribute__((__unused__)) va, char *buffer)
+int print_por(va_list __attribute__((__unused__)) va, char *buffer)
 {
 	*buffer = '%';
 
